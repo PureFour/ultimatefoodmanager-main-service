f937720e09fdb1a8502702b4913a53d25c7c899c
daniel <daniel.baziak@student.uj.edu.pl>
2021-01-04 20:50:16 +0100

    media-service integration fix

--- src/main/java/com/purefour/mainservice/feign/MediaServiceClient.java
+++ src/main/java/com/purefour/mainservice/feign/MediaServiceClient.java
@@ -1,12 +1,17 @@
 package com.purefour.mainservice.feign;
 
 import com.purefour.mainservice.feign.decoder.CommonErrorDecoder;
+import com.purefour.mainservice.model.product.ImageModel;
 import feign.Feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(
         name = "media-service",
@@ -16,14 +21,32 @@ import org.springframework.web.bind.annotation.RequestHeader;
 )
 public interface MediaServiceClient {
 
-    @GetMapping("helloWorld")
-    String helloWorld(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader);
+    //TODO tutaj lepiej zrobić wrzucanie ImageModel!
+
+    @PostMapping("images/upload")
+    ImageModel saveImage(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, MultipartFile imageFile);
+
+    @GetMapping("images/{uuid}")
+    ImageModel getImage(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @PathVariable("uuid") String uuid);
+
+    @DeleteMapping("images/{uuid}")
+    String deleteImage(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @PathVariable("uuid") String uuid);
 
     class MediaServiceClientFallback implements MediaServiceClient {
         private static final String SERVICE_UNAVAILABLE_MSG = "MediaService unavailable.";
 
         @Override
-        public String helloWorld(String authorizationHeader) {
+        public ImageModel saveImage(String authorizationHeader, MultipartFile imageFile) {
+            throw new IllegalStateException(SERVICE_UNAVAILABLE_MSG);
+        }
+
+        @Override
+        public ImageModel getImage(String authorizationHeader, String uuid) {
+            throw new IllegalStateException(SERVICE_UNAVAILABLE_MSG);
+        }
+
+        @Override
+        public String deleteImage(String authorizationHeader, String uuid) {
             throw new IllegalStateException(SERVICE_UNAVAILABLE_MSG);
         }
     }
---
+++ src/main/java/com/purefour/mainservice/model/product/ImageModel.java
@@ -0,0 +1,19 @@
+package com.purefour.mainservice.model.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImageModel {
+
+    private UUID uuid;
+    private String name;
+    private byte[] bytes;
+}
